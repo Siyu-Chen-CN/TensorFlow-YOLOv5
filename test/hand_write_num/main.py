a08fa0cbb011968761ebbd82af6ac3ebcb77f84e
Siyu <siyuchen0015@gmail.com>
2024-07-20 03:11:16 +0200

    update hand write code

--- test/hand_write_num/main.py
+++ test/hand_write_num/main.py
@@ -1,13 +1,47 @@
 # Classification
+import os
 import tensorflow as tf
-from keras import datasets, optimizers
-from tensorflow.python import layers
+import keras
+from keras import datasets, optimizers, layers
 
-(xs, ys), _ = datasets.mnist.load_data()
-print("datasets:", xs.shape, ys.shape)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-xs = tf.convert_to_tensor(xs, dtype=tf.float32) / 255.
-db = tf.data.Dataset.from_tensor_slices((xs, ys))
+#加载数据集
+(x, y), (x_val, y_val) = datasets.mnist.load_data()
+x = tf.convert_to_tensor(x, dtype=tf.float32) / 255.
+y = tf.convert_to_tensor(y, dtype=tf.int32)
+y = tf.one_hot(y, depth=10)
+db = tf.data.Dataset.from_tensor_slices((x, y))
+db = db.batch(200)
 
-for step, (x,y) in enumerate(db):
-    print(step, x.shape, y, y.shape)
+
+# 准备结构
+model = keras.Sequential()
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(10))
+
+optimizer = optimizers.SGD(learning_rate=0.001)
+
+
+def train_epoch(epoch):
+    with tf.GradientTape() as tape:
+        for step, (x, y) in enumerate(db):
+            x = tf.reshape(x, (-1, 28*28))
+            out = model(x)
+            loss = tf.reduce_sum(tf.square(out - y)) / x.shape[0]
+
+        grads = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+        if step % 100 == 0:
+            print(epoch, step, 'loss:', loss.numpy())
+
+
+def train():
+    for epoch in range(30):
+        train_epoch(epoch)
+
+
+if __name__ == '__main__':
+    train()
